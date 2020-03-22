9f1d91eb31d02edbad588a9bce340c685fef4bf3
Xemicolon <1vibenation@gmail.com>
2020-03-22 01:17:07 +0100

    JavaScript Task 1

--- Javascript/index.js
+++ Javascript/index.js
@@ -4,20 +4,19 @@ var name = "Oluwatobiloba Light";
 // Store your courses in an array.
 var courses = ["HTML", "CSS", "JS", "NodeJS", "Python"];
 
+// Display your name and courses and do one of the following:
 console.log("My name is " + name + " and I am taking the courses below:");
 
-// Display the name of courses
 for (var i = 0; i < courses.length; i++) {
   console.log(courses[i]);
 }
 
-// Display your name and courses and do one of the following:
 // 1. If the number of courses you are doing is an odd number, find and display all odd numbers from 1-200 (inclusive)
 
 if (courses.length % 2 !== 0) {
   // find and display all odd numbers from 1 - 200
   console.log(
-    "The number of courses I am taking is even and the even numbers from 1 - 200 are:"
+    "The number of courses I am taking is odd and the odd numbers from 1 - 200 are:"
   );
   for (var i = 0; i <= 200; i++) {
     if (i % 2 !== 0) {
